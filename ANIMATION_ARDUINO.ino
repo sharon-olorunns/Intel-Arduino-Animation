6f6a2b61450862d4410ab927e8ac4f258cb143c5
sharon-olorunns <olorunns@tcd.ie>
2017-07-20 10:20:19 +0100

    Add files via upload

---
+++ ANIMATION_ARDUINO.ino
@@ -0,0 +1,73 @@
+/* Jumper: Using an Arduino to animate:
+  Written by ScottC on 02/06/2012 */
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
+
+int photoRPin = 0;
+int minLight;
+int maxLight;
+int lightLevel;
+int adjustedLightLevel;
+int oldLightLevel;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  lightLevel = analogRead(photoRPin);
+  minLight = lightLevel - 10;
+  maxLight = lightLevel;
+  oldLightLevel = lightLevel;
+
+
+}
+
+void loop() {
+
+  lightLevel = analogRead(photoRPin);
+  delay(10);
+
+  //auto-adjust the minimum and maximum limits in real time
+  if (minLight > lightLevel) {
+    minLight = lightLevel;
+  }
+  if (maxLight < lightLevel) {
+    maxLight = lightLevel;
+  }
+
+  //Map the light level to produce a result between 1 and 28.
+  adjustedLightLevel = map(lightLevel, (minLight + 20), (maxLight - 20), 1, 28);
+  adjustedLightLevel = constrain (adjustedLightLevel, 1, 28);
+  lcd.begin(0, 0);
+  lcd.print("Adjusted LL: ");
+  lcd.print(adjustedLightLevel);
+
+  if ( adjustedLightLevel == 28) {
+    digitalWrite(2, HIGH);
+    int r = 25;
+    int g = 200;
+    int b = 160;
+    lcd.setRGB(r, g, b);
+
+  }
+  else {
+    digitalWrite(2, LOW);
+
+
+  }
+
+  /*Only send a new value to the Serial Port if the
+    adjustedLightLevel value changes.*/
+  if (oldLightLevel == adjustedLightLevel) {
+    //do nothing if the old value and the new value are the same.
+  } else {
+    //Update the oldLightLevel value for the next round
+    oldLightLevel = adjustedLightLevel;
+
+    /*Send the adjusted Light level result
+      to Serial port (processing)*/
+    Serial.println(adjustedLightLevel);
+  }
+} 
---
+++ Animation_Arduino.pde
@@ -0,0 +1,65 @@
+/* Jumper: Using an Arduino to animate
+ Written by ScottC on 02/06/2012
+ 
+ Source code derived from : 
+ http://processing.org/learning/topics/sequential.html
+ http://processing.org/discourse/beta/num_1267080062.html
+ 
+ Pictures captured from:
+ http://www.youtube.com/watch?v=h6nE8m74kDg      
+ 
+ ======================================================= */
+
+import processing.serial.*;
+Serial myPort;
+String sensorReading="";
+
+// Create the array that will hold the images
+PImage[] movieImage = new PImage[31];
+
+/* The frame variable is  used to control which 
+ image is displayed */
+int frame = 1;
+
+
+void setup() {
+  fullScreen();
+
+  myPort = new Serial(this, "COM4", 9600);
+  myPort.bufferUntil('\n');
+
+  for (int i=0; i<30; i++) {
+    movieImage[i] = loadImage("Screenshot 1 (" + (i+1) + ").png");
+  }
+}
+
+void draw() {
+ 
+  image(movieImage[frame-1], 0, 0, width, height);
+}
+
+void serialEvent (Serial myPort) {
+  sensorReading = myPort.readStringUntil('\n');
+  if (sensorReading != null) {
+    sensorReading=trim(sensorReading);
+    if (sensorReading.length()<2) {
+      frame = integerFromChar(sensorReading.charAt(0));
+    } else {
+      frame = integerFromChar(sensorReading.charAt(0))*10;
+      frame += integerFromChar(sensorReading.charAt(1));
+    }
+  }
+}
+
+
+
+/* This function used to convert the character received from the
+ serial port (Arduino), and converts it to a number */
+
+int integerFromChar(char myChar) {
+  if (myChar < '0' || myChar > '9') {
+    return -1;
+  } else {
+    return myChar - '0';
+  }
+}
